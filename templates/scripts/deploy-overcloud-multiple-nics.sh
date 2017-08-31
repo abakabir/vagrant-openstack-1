1cf9780cead81db68c08152db0de228105ffa4c8
Homero Pawlowski <homeski2@gmail.com>
2017-08-30 23:28:50 -0700

    added network-isolation into templates

--- templates/scripts/deploy-overcloud-multiple-nics.sh
+++ templates/scripts/deploy-overcloud-multiple-nics.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+. /home/stack/stackrc
+
 time openstack overcloud deploy --templates \
   -e /home/stack/templates/environment/network-environment-multiple-nics.yaml \
-  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+  -e /home/stack/templates/environment/network-isolation.yaml \
   -e /home/stack/templates/environment/storage-environment.yaml \
   -e /home/stack/templates/environment/services.yaml \
   -e /home/stack/templates/environment/low-memory-usage.yaml \
